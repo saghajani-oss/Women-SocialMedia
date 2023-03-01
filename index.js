f3f5038f3a595d53a8d8e80e9267775cfd320d9b
saghajani-oss <112084533+saghajani-oss@users.noreply.github.com>
2023-03-02 00:02:17 +0330

    Adding JS for Sidebar

---
+++ index.js
@@ -0,0 +1,17 @@
+//SIDEBAR
+const menuItems = document.querySelectorAll(".menu-item");
+
+//remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove("active");
+    })
+}
+
+menuItems.forEach(item =>{
+    item.addEventListener("click", ()=> {
+        changeActiveItem();
+        item.classList.add("active");
+    })
+})
+
